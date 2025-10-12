3b3aef6b8b309aaff9047834de45ecf69ce6c821
ToBi <lehoaiduybinh.pch.2b7.97@gmail.com>
2025-10-12 14:50:44 +0700

    Updated learning

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 # from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail
+from watchlist_app.api.views import (WatchListAV, WatchDetailAV, StreamPlatformAV, 
+                                     StreamPlatformDetailAV, ReviewList, 
+                                     ReviewDetail)
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
@@ -8,6 +10,6 @@ urlpatterns = [
 
     path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
     path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
-    path('review', ReviewList.as_view(), name='review-list'),
+    path('review/', ReviewList.as_view(), name='review-list'),
     path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail')
 ]
